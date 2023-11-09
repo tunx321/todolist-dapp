7816445a3d920aa267bb2079cd671b60d9ed1b5e
tungeekx <almatx78@gmail.com>
2023-11-10 00:00:05 +0600

    deploy conf

---
+++ contracts/Todo.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Todo {
+    uint public taskId;
+
+    event AddTask(address recipient, uint taskId);
+    event CompleteTask(uint taskId, bool completed);
+
+    struct Task {
+        uint id;
+        string body;
+        bool completed;
+    }
+
+    mapping(address => Task[]) private Users;
+
+    function addTask(string memory body) external {
+        Users[msg.sender].push(
+            Task({id: taskId, body: body, completed: false})
+        );
+
+        emit AddTask(msg.sender, taskId);
+    }
+
+    function getMyTasks() external view returns (Task[] memory) {
+        return Users[msg.sender];
+    }
+
+    function completeTask(uint _taskId) external {
+        require(_taskId < Users[msg.sender].length, "No task has been added"); 
+        Users[msg.sender][_taskId].completed = true;
+        emit CompleteTask(_taskId, true);
+    }
+
+    function deleteTask(uint _taskId) external {
+        delete Users[msg.sender][_taskId];
+    }
+
+    function getTaskCount() external view returns (uint256) {
+        return Users[msg.sender].length;
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Todo = await hre.ethers.getContractFactory("Todo")
+  const todo = await Todo.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await todo.waitForDeployment()
+  console.log("Todo: ", await todo.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
